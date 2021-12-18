124c7d6a4825a4db9435c900adddce24a4958ccd
MarcosWitcel <joao.marcos.witcel@gmail.com>
2021-12-18 14:41:49 -0300

    solução do exercício 21

---
+++ 01 Prolog Lists/p1.21.pl
@@ -0,0 +1,25 @@
+/*
+
+Insert an element at a given position into a list.
+Example:
+?- insert_at(alfa,[a,b,c,d],2,L).
+L = [a,alfa,b,c,d]
+
+*/
+
+:- use_module(library(clpfd)).
+
+insert_at(V, Ls, At, NLs) :-
+    insert_at(V, Ls, At, NLs, []).
+
+insert_at(_, [], _, Acc, Acc).
+insert_at(V, [Head|Tail], At0, NLs, Acc0) :-
+    At1 #= At0 - 1,
+	( At0 #= 1 ->
+	    append(Acc0, [V, Head], Acc1),
+	    insert_at(V, Tail, At1, NLs, Acc1)
+	;
+	    append(Acc0, [Head], Acc1),
+	    insert_at(V, Tail, At1, NLs, Acc1)
+	).
+    
