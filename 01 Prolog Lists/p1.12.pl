72fdf91a1222fb5f2cd5037edd56d7f64d10a906
MarcosWitcel <joao.marcos.witcel@gmail.com>
2021-12-28 13:10:11 -0300

    solução do exercício 12

---
+++ 01 Prolog Lists/p1.12.pl
@@ -0,0 +1,23 @@
+/*
+
+Decode a run-length encoded list.
+Given a run-length code list generated as specified in problem 1.11. Construct its uncompressed version.
+
+*/
+
+decode(ELs, Ls) :-
+    decode_(ELs, Ls, []).
+
+returnAtom(Atom, Atom).
+	
+decode_([], Acc, Acc).
+decode_([Head|Tail], Ls, Acc0) :-
+    (atom(Head) ->
+	    append(Acc0, [Head], Acc)
+	;
+	    =([Length, Atom], Head),
+		length(List, Length),
+		maplist(returnAtom(Atom), List),
+	    append(Acc0, List, Acc)
+	),
+    decode_(Tail, Ls, Acc).
