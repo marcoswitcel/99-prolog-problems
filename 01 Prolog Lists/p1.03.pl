386212f9b3dfbc628dd4eadb6b9205bd301afc99
MarcosWitcel <joao.marcos.witcel@gmail.com>
2022-01-01 16:55:09 -0300

    revisão na questão 3

--- 01 Prolog Lists/p1.03.pl
+++ 01 Prolog Lists/p1.03.pl
@@ -6,11 +6,23 @@ Example:
 X = c
 */
 
+test(K) :- K = 1, integer(K).
 
 list_element_k(Ls, Element, K) :-
-    list_element_k_(Ls, Element, K, 1).
-	
+    var(K),
+    list_element_k_(Ls, Element, K, 1);
+	integer(K),
+	list_element_kb_(Ls, Element, K, 1).
+
+% unbound K element	
 list_element_k_([Head|_], Head, K, K).
 list_element_k_([_|Tail], Element, K, Acc) :-
     AccNext is Acc + 1,
     list_element_k_(Tail, Element, K, AccNext).
+
+% bound K element
+list_element_kb_([Head|_], Head, K, K).
+list_element_kb_([_|Tail], Element, K, Acc) :-
+    K =\= Acc,
+    AccNext is Acc + 1,
+    list_element_kb_(Tail, Element, K, AccNext).
